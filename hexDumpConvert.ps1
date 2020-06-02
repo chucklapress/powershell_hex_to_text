f861da293fb2058c9a5514d4f70f2e33b613673c
chucklapress <charleslapress@gmail.com>
2020-06-01 20:07:55 -0400

    script to convert hexdump to text

---
+++ hexDumpConvert.ps1
@@ -0,0 +1,8 @@
+# powershell to convert Hex dump to text
+#$hex = "54 68 69 73 20 69 73 20 61 20 48 65 78 44 75 6d 70 20 6e 6f 74 20 61 6c 6c 20 6c 61 64 69 65 73 20 62 72 61 20 73 69 7a 65 73"
+#$hex.Split(" ") | forEach {[char]([convert]::toint16($_,16))} | forEach {$result = $result + $_}
+#$result
+
+$hex = Read-Host "enter your hex dump: "
+$hex.Split(" ") | forEach {[char]([convert]::toint16($_,16))} | forEach {$result = $result + $_}
+Write-Host "Here are your results:"$result
